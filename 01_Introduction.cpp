f95cea25b5a2622cbdf62e2df12a018415103a6c
subham <dark.demon.das@gmail.com>
2023-11-24 20:09:58 +0530

    Covered the introduction part of Polymorphism in C++

---
+++ 01_Introduction.cpp
@@ -0,0 +1,7 @@
+/*
+    -> The word “polymorphism” means having many forms where "Poly" means "Many" and "Morph" means "Form".
+    ->  It is the ability of the objects to take different forms. 
+    -> It indicates that the form of a member function can be changed at run-time. Such member functions are called "Virtual Functions" and the corresponding class is called "Polymorphic Class".
+    -> The "Virtual function" allow programmers to declare functions in a base class and redefine the same with same name in its derieved classes.
+    -> The additional feature of a "Virtual function" is that the compiler and linker will guarantee the execution ofthe correct function associated with the objects of each of the derived class at run-time.
+*/
